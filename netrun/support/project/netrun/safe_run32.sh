9d43d8b8f5740324dd3400c0552790b0149855c9
Orion Lawlor <lawlor@alaska.edu>
2018-12-05 15:15:12 -0900

    Added safe_run32 for 32-bit libraries.

---
+++ netrun/support/project/netrun/safe_run32.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# New version, calling "s4g_chroot" which builds "run" directory,
+#  enforces time limit, and enforces process limit.
+# This is faster (no perl), more reliable (no zombies), and cleaner than below.
+if [ ! -r run/proc/stat ]
+then
+  mkdir -p run/proc
+  if [ -r /proc/stat ]
+  then
+	cp /proc/stat run/proc/stat
+  else
+	echo -e "cpu\ncpu0\ncpu1" > run/proc/stat
+  fi
+fi
+mkdir -p run/etc
+mkdir -p run/dev
+dd if=/dev/urandom of=run/dev/urandom bs=4k count=1 > /dev/null 2>&1
+
+exec /usr/local/bin/s4g32_chroot "$@"
